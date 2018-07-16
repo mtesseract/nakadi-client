8018d1c85e331bd561c75dfafe960c9f6517bb26
Moritz Clasmeier <mtesseract@silverratio.net>
2018-07-16 22:40:41 +0200

    Implement modifyGlobalConfiguration and added Haddock comments

--- src/Network/Nakadi/Unsafe/IO.hs
+++ src/Network/Nakadi/Unsafe/IO.hs
@@ -1,6 +1,23 @@
+{-|
+Module      : Network.Nakadi.Unsafe.IO
+Description : Support for MonadNakadi IO Instance
+Copyright   : (c) Moritz Clasmeier 2018
+License     : BSD3
+Maintainer  : mtesseract@silverratio.net
+Stability   : experimental
+Portability : POSIX
+
+Nakadi-client provides a 'MonadNakadi' instance for the 'IO' monad
+accessing a global mutable configuration.
+
+This module exports the user-visible API for managing this global
+configuration.
+-}
+
 module Network.Nakadi.Unsafe.IO
   ( initializeGlobalConfigurationFromEnv
   , setGlobalConfiguration
+  , modifyGlobalConfiguration
   )
 where
 
